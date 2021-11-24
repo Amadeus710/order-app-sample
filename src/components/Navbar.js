6219ed0377a7f9f6b1628308fbc4c904332589db
Amadeus710 <69098664+Amadeus710@users.noreply.github.com>
2021-11-23 23:32:52 -0500

    finishing up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const Click = () => {
+    setShowLinks(!showLinks);
+  };
   return (
     <div>
       <div className='navbar'>
-        <div className='leftSide'>
+        <div className='leftSide' id={showLinks ? "open" : "close"}>
           <img src={logo} />
+          <div className='hiddenLinks'>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Menu</Link>
+            <Link to='/'>About</Link>
+            <Link to='/'>Contact</Link>
+          </div>
         </div>
 
         <div className='rightSide'>
@@ -17,6 +28,9 @@ const Navbar = () => {
           <Link to='/'>Menu</Link>
           <Link to='/'>About</Link>
           <Link to='/'>Contact</Link>
+          <button onClick={Click}>
+            <ReorderIcon />
+          </button>
         </div>
       </div>
     </div>
